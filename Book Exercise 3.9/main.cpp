59594dfb5d97a36c72873c950e1180b3a4adbdb6
alexberry721 <alexberry721@gmail.com>
2016-08-31 11:06:41 -0700

    Initial Commit

---
+++ Book Exercise 3.9/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Book Exercise 3.9
+//
+//  Created by ax on 8/31/16.
+//  Copyright © 2016 COMP130. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
